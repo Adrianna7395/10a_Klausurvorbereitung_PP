48c803686185824da1bb1211ff3ecb9e8f5bcb2d
Adriana <hrl.davidson@gmail.com>
2022-02-08 08:34:38 +0100

    teil 1

--- funktionen.js
+++ funktionen.js
@@ -1,2 +1,27 @@
 /* DATENSTRUKTUREN */
 
+// Primitives
+
+let a // Definition oder Deklaration
+
+// Wertzuweisung, Assignment
+a = true; //boolean (true, false)
+a = "Hello"; //datentyp String
+a = 2; // datentyp Number
+
+console.log(typeof a); // welche typ
+console.log (a); 
+
+// Arrays
+let arr;
+arr = new Array();
+arr = []; // leeres Array
+arr = [true,false,false,true]; // Array mit boolean Werten
+
+// Index beginnt mit 0
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[3]);
+console.log(arr[arr.length]); //lenght of Array
+console.log(arr[arr.length-1]);
+console.log(arr[5]);
